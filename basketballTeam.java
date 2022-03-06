6cb663f4b113cd939f0a3e3bb4d9f8fb5674b5e7
clintonofo <clintonofodile7@outlook.ie>
2022-03-06 15:00:38 +0000

    commit 2

--- basketballTeam.java
+++ basketballTeam.java
@@ -1,63 +1,134 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.Scanner;
 public class basketballTeam {
 
     public static void main(String[] args) {
         // TODO Auto-generated method stub
 
-        Scanner input = new Scanner(System.in);
-        boolean mainLoop = true;
-
-        int choice;
-        do {
-            System.out.println("Main Menu\n");
-            System.out.print("1.) Display All Elements \n");
-            System.out.print("2.) Display All Ele.\n");
-            System.out.print("3.) Multiplication.\n");
-            System.out.print("4.) Division.\n");
-            System.out.print("5.) Generate Random Number.\n");
-            System.out.print("6.) Exit\n");
-            System.out.print("\nEnter Your Menu Choice: ");
-
-            choice = input.nextInt();
 
 
-        }
+            Scanner input = new Scanner(System.in);
+            boolean mainLoop = true;
 
-        while (choice > 7);
+            int choice;
+            do {
+                System.out.println("Main Menu\n");
+                System.out.print("1.) Display All Elements \n");
+                System.out.print("2.) Choose key\n");
+                System.out.print("3.) Multiplication.\n");
+                System.out.print("4.) Division.\n");
+                System.out.print("5.) Generate Random Number.\n");
+                System.out.print("6.) Exit\n");
+                System.out.print("\nEnter Your Menu Choice: ");
 
-        switch (choice) {
-            case 1:
+                choice = input.nextInt();
+
+
+            }
+
+            while (choice > 7);
+            if (choice == 1) {
+
                 ArrayList<team> team = new ArrayList<team>();
 
                 // Initialize an ArrayList with add()
-               team.add(new team("John","moons",15000,"ben","sam,","atlanta"));
-                team.add(new team("Eoin","rockets",25000,"john","oliver,","san fracnsio"));
-                team.add(new team("liam","spices",35000,"ben","sam,","philly"));
-                team.add(new team("clinton","ice",5000,"rita","sammy,","la"));
-                team.add(new team("allen","lakers",15000,"luke","sam,","la"));
-                team.add(new team("barry","warriors",25000,"benson","jamal,","golden state"));
-                team.add(new team("lia","clippers",65000,"ella","rio,","charlotte"));
-                team.add(new team("rhia","zooms",15000,"lia","clinton,","cleveland"));
-                team.add(new team("clinton","heat",10000,"elsa","ronaldo,","chicagi"));
-                team.add(new team("demar","raptors",20000,"leon","samuel,","toronto"));
+                team.add(new team("John", "moons", 15000, "ben", "sam,", "atlanta"));
+                team.add(new team("Eoin", "rockets", 25000, "john", "oliver,", "san fracnsio"));
+                team.add(new team("liam", "spices", 35000, "ben", "sam,", "philly"));
+                team.add(new team("clinton", "ice", 5000, "rita", "sammy,", "la"));
+                team.add(new team("allen", "lakers", 15000, "luke", "sam,", "la"));
+                team.add(new team("barry", "warriors", 25000, "benson", "jamal,", "golden state"));
+                team.add(new team("lia", "clippers", 65000, "ella", "rio,", "charlotte"));
+                team.add(new team("rhia", "zooms", 15000, "lia", "clinton,", "cleveland"));
+                team.add(new team("clinton", "heat", 10000, "elsa", "ronaldo,", "chicagi"));
+                team.add(new team("demar", "raptors", 20000, "leon", "samuel,", "toronto"));
 
-                for(team t: team) {
+                for (team t : team) {
                     System.out.println(t);  // Will invoke overrided `toString()` method
                 }
-            case 2:
+            }
 
-                };
+               else if (choice == 2)
+            {
+                    HashMap<String, team> bballTeam = new HashMap<String, team>();
+                    team t1 = new team("John", "moons", 15000, "ben", "sam,", "atlanta");
+                    team t2 = new team("Eoin", "rockets", 25000, "john", "oliver,", "san fracnsio");
+                    team t3 = new team("liam", "spices", 35000, "ben", "sam,", "philly");
+                    team t4 = new team("clinton", "ice", 5000, "rita", "sammy,", "la");
+                    team t5 = new team("allen", "lakers", 15000, "luke", "sam,", "la");
+                    team t6 = new team("barry", "warriors", 25000, "benson", "jamal,", "golden state");
+                    team t7 = new team("lia", "clippers", 65000, "ella", "rio,", "charlotte");
+                    team t8 = new team("rhia", "zooms", 15000, "lia", "clinton,", "cleveland");
+                    team t9 = new team("clinton", "heat", 10000, "elsa", "ronaldo,", "chicagi");
+                    team t10 = new team("demar", "raptors", 20000, "leon", "samuel,", "toronto");
 
 
+                    // Add keys and values (Country, City)
+                    bballTeam.put(t1.getPlayerName(), t1);
+                    bballTeam.put(t2.getPlayerName(), t2);
+                    bballTeam.put(t3.getPlayerName(), t3);
+                    bballTeam.put(t4.getPlayerName(), t4);
+                    bballTeam.put(t5.getPlayerName(), t5);
+                    bballTeam.put(t6.getPlayerName(), t6);
+                    bballTeam.put(t7.getPlayerName(), t7);
+                    bballTeam.put(t8.getPlayerName(), t8);
+                    bballTeam.put(t9.getPlayerName(), t9);
+                    bballTeam.put(t10.getPlayerName(), t10);
 
 
+                    String choice2 = "";
+                    while (!choice2.equalsIgnoreCase("back")) {
+                        System.out.println("Choose Key 1 - john");
+                        System.out.println("Choose Key 2 - eoin");
+                        System.out.println("Choose Key 3 - liam");
+                        System.out.println("Choose Key 4 - clinton");
+                        System.out.println("Choose Key 5 - allen");
+                        System.out.println("Choose Key 6 - barry");
+                        System.out.println("Choose Key 7 - lia");
+                        System.out.println("Choose Key 8 - rhia");
+                        System.out.println("Choose Key 9 - clinton");
+                        System.out.println("Choose Key 10 - demar");
+                        System.out.println("back - back");
+
+                        choice2 = input.nextLine();
+
+                        if (choice2.equalsIgnoreCase("john")) {
+
+                            System.out.println(bballTeam.get(t1.getPlayerName()));
+                        } else if (choice2.equalsIgnoreCase("eoin")) {
+                            System.out.println(bballTeam.get(t2.getPlayerName()));
+                        } else if (choice2.equalsIgnoreCase("liam")) {
+                            System.out.println(bballTeam.get(t3.getPlayerName()));
+                        } else if (choice2.equalsIgnoreCase("clinton")) {
+                            System.out.println(bballTeam.get(t4.getPlayerName()));
+                        } else if (choice2.equalsIgnoreCase("allen")) {
+                            System.out.println(bballTeam.get(t5.getPlayerName()));
+                        } else if (choice2.equalsIgnoreCase("barry")) {
+                            System.out.println(bballTeam.get(t6.getPlayerName()));
+                        } else if (choice2.equalsIgnoreCase("lia")) {
+                            System.out.println(bballTeam.get(t7.getPlayerName()));
+                        } else if (choice2.equalsIgnoreCase("rhia")) {
+                            System.out.println(bballTeam.get(t8.getPlayerName()));
+                        } else if (choice2.equalsIgnoreCase("clinton")) {
+                            System.out.println(bballTeam.get(t9.getPlayerName()));
+                        } else if (choice2.equalsIgnoreCase("demar")) {
+                            System.out.println(bballTeam.get(t10.getPlayerName()));
+                        } else if (choice2.equalsIgnoreCase("back")) {
+                            break;
+
+                        }
+                        else{
+                            System.out.println("Wrong input");
+                        }
 
 
+                    }
+                    ;
 
 
-
-
-
+            }
         }
     }
+
+
--- team.java
+++ team.java
@@ -21,6 +21,55 @@ public class team implements Comparable<team> {
         this.city = city;
 
     }
+
+    public String getPlayerName() {
+        return playerName;
+    }
+
+    public void setPlayerName(String playerName) {
+        this.playerName = playerName;
+    }
+
+    public String getTeamName() {
+        return teamName;
+    }
+
+    public void setTeamName(String teamName) {
+        this.teamName = teamName;
+    }
+
+    public int getSalary() {
+        return salary;
+    }
+
+    public void setSalary(int salary) {
+        this.salary = salary;
+    }
+
+    public String getManager() {
+        return manager;
+    }
+
+    public void setManager(String manager) {
+        this.manager = manager;
+    }
+
+    public String getCoach() {
+        return coach;
+    }
+
+    public void setCoach(String coach) {
+        this.coach = coach;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
     @Override
     public String toString() {
         return "team{" +
@@ -31,6 +80,19 @@ public class team implements Comparable<team> {
                 ", coach='" + coach + '\'' +
                 ", city='" + city + '\'' +
                 '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        team team = (team) o;
+        return salary == team.salary && Objects.equals(playerName, team.playerName) && Objects.equals(teamName, team.teamName) && Objects.equals(manager, team.manager) && Objects.equals(coach, team.coach) && Objects.equals(city, team.city);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(playerName, teamName, salary, manager, coach, city);
     }
 
     @Override
